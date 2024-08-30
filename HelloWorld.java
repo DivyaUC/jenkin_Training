42d1776b40f76448b1ce5d2979f584a1e3115b5b
DivyaUC <137720055+DivyaUC@users.noreply.github.com>
2024-08-30 12:53:08 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+Public class Hello World{
+static void method(){
+System.out.println("Hello Java Method");
+System.out.println("Hello changed Java method");
+}
+public static void main(String[] args){
+System.out.println("Hello java worldd");
+method();
+}
+}
