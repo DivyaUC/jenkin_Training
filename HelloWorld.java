616db6eb58023da06e3cc8e62233b619cd62c036
DivyaUC <divya.challawar@cumminscollege.in>
2024-08-30 13:16:03 +0530

    the new changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,13 @@ static void method(){
 System.out.println("Hello Java Method");
 System.out.println("Hello changed Java method");
 }
+static void method2(){
+System.out.println("Hello Java Method");
+System.out.println("Hello changed Java method2");
+}
 public static void main(String[] args){
 System.out.println("Hello java worldd");
 method();
+method2();
 }
 }
